72b4142c5445ab76f96b48af845538472b39eba8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-02 12:43:35 -0500

    [Green]: returns 'Error' for -12

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,6 +3,7 @@
 export function fizzbuzz (num: number) {
     const isAMultipleOfThree = num % 3 === 0;
     if (num > 100) return "Error"
+    if (num < 0) return "Error"
     if (num === 43) return "43"
     if (num === 15) return "FizzBuzz"
     if (num === 45) return "FizzBuzz"
