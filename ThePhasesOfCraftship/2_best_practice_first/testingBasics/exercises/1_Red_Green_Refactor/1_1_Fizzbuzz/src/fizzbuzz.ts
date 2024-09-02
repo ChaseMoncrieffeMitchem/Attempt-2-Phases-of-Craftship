5696ab1c94cfd1e4c254ebb7d99df53f0ac8222c
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-02 12:12:34 -0500

    [Green]: returns '43' for 43

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,6 +2,7 @@
 
 export function fizzbuzz (num: number) {
     const isAMultipleOfThree = num % 3 === 0;
+    if (num === 43) return "43"
     if (num === 15) return "FizzBuzz"
     if (isAMultipleOfThree) return "Fizz"
     return "Buzz"
