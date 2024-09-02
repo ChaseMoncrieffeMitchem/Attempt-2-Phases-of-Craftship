3da244aa3787d73cacf45ac105dc63191c0abcd7
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-02 12:50:58 -0500

    [Refactor]: refactored function to reduce duplication

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,11 +2,11 @@
 
 export function fizzbuzz (num: number) {
     const isAMultipleOfThree = num % 3 === 0;
+    const isAMultipleOfFive = num % 5 === 0;
     if (num > 100) return "Error"
     if (num < 0) return "Error"
-    if (num === 43) return "43"
-    if (num === 15) return "FizzBuzz"
-    if (num === 45) return "FizzBuzz"
+    if (isAMultipleOfFive && isAMultipleOfThree) return "FizzBuzz"
     if (isAMultipleOfThree) return "Fizz"
-    return "Buzz"
+    if (isAMultipleOfFive) return "Buzz"
+    return num.toString()
 }
