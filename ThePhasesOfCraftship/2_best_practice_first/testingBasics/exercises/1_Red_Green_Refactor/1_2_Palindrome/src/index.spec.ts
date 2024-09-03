a0eb75d2d425b96e8ec10501c881878002f06d79
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-02 19:01:22 -0500

    [Red]: returns true for 'mom'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,23 @@
+// 🔘 I have committed on every single transition from red to green to refactor
+// 🔘 I have tests that validate the following statements 
+// "mom" returns true
+// "Mom" returns true
+// "MoM" returns true
+// "Momx" returns false
+// "xMomx" returns true
+// "Was It A Rat I Saw" returns true
+// "Never Odd or Even" returns true
+// "Never Odd or Even1" returns false 
+// "1Never Odd or Even1" returns true
+// 🔘 Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization (see Tip #3 here)
+// 🔘 There is no duplication in my test code or my production code
+
+import { palindromeChecker } from "./palindromeChecker"
 
 describe('palindrome checker', () => {
+
+    it('returns true for str "mom"', () => {
+        expect(palindromeChecker("mom")).toBe(true)
+    })
 
 })
