4cbe021ba2793584b3a334f73148195faafe2908
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 14:24:52 -0500

    [Green]: knows that '53' is the smallest number in the array

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/statsCalculator.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/statsCalculator.ts
@@ -1,13 +1,12 @@
 export class statsCalculator {
 
     public static validate (input: number[]) {
-        input = [2, 4, 21, -8, 53, 40]
         const smallestInteger = input.reduce((min, current) => current < min ? current : min)
 
         return {
             minNum: smallestInteger,
-            maxNum: 53,
-            arrNum: 1
+            // maxNum: 53,
+            // arrNum: 1
         }
     }
 
