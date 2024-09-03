8a272d3fe683c399746060220fa9466a84f3b470
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 14:33:44 -0500

    [Refactor]: identified higher level abstraction that we need to find the smallest number in the array, parameterization

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,23 +2,33 @@ import { statsCalculator } from "./statsCalculator";
 
 describe('stats calculator', () => {
 
-    it("knows that '-8' is the smallest number in the array", () => {
-        let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
-
-        expect(output.minNum).toBe(-8)
+    describe("Identifying the smallest integer given an array of integers", () => {
+        it.each([
+            [[2, 4, 21, -8, 53, 40], -8],
+            [[53, 88, 94, 70, 90, 100], 53],
+            [[53, 88, 4, 2, 90, 100], 2]
+        ])("%s returns %s", (input: number[], result: number) => {
+            let output = statsCalculator.validate(input)
+            expect(output.minNum).toBe(result)
+        })
     })
+        // it("knows that '-8' is the smallest number in the array", () => {
+        //     let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
 
-    it("knows that '53' is the smallest number in the array", () => {
-        let output = statsCalculator.validate([53, 88, 94, 70, 90, 100])
+        //     expect(output.minNum).toBe(-8)
+        // })
 
-        expect(output.minNum).toBe(53)
-    })
+    //     it("knows that '53' is the smallest number in the array", () => {
+    //         let output = statsCalculator.validate([53, 88, 94, 70, 90, 100])
 
-    it("knows that '2' is the smallest number in the array", () => {
-        let output = statsCalculator.validate([53, 88, 4, 2, 90, 100])
+    //         expect(output.minNum).toBe(53)
+    //     })
 
-        expect(output.minNum).toBe(2)
-    })
+    //     it("knows that '2' is the smallest number in the array", () => {
+    //         let output = statsCalculator.validate([53, 88, 4, 2, 90, 100])
+
+    //         expect(output.minNum).toBe(2)
+    // })
 
     // it("knows that '53' is the biggest number in the array", () => {
     //     let output = statsCalculator.validate([53])
