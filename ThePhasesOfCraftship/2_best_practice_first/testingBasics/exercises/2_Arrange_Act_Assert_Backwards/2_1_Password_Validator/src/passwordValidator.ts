968cd069d1bb38798e5e9caae6161a2a135e8df2
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 00:57:52 -0500

    [Refactor]: refactored test code to reduce conditional duplication

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/passwordValidator.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/passwordValidator.ts
@@ -1,5 +1,14 @@
 export function passwordValidator(str: string) {
-    if (str === "mom555") return true
-    if (str === "mom") return false
-    if (str === "mommy") return false
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    const passwordLength = str.length
+    const hasDigit = digits.some(digit => str.includes(digit))
+    const isValidLength = passwordLength >= 5 && passwordLength <= 15
+
+    if (!isValidLength) return false
+    if (!hasDigit) return false
+    return true
+
+    // if (str === "mom555") return true
+    // if (str === "mom") return false
+    // if (str === "mommy") return false
 }
