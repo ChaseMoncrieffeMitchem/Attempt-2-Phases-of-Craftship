feb87c033cd9178e619c7759f28f182b3c9cc3b3
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 14:47:56 -0500

    [Green]: knows that '53' is the biggest number in the array

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -12,29 +12,12 @@ describe('stats calculator', () => {
             expect(output.minNum).toBe(result)
         })
     })
-        // it("knows that '-8' is the smallest number in the array", () => {
-        //     let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
 
-        //     expect(output.minNum).toBe(-8)
-        // })
+    it("knows that '53' is the biggest number in the array", () => {
+        let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
 
-    //     it("knows that '53' is the smallest number in the array", () => {
-    //         let output = statsCalculator.validate([53, 88, 94, 70, 90, 100])
-
-    //         expect(output.minNum).toBe(53)
-    //     })
-
-    //     it("knows that '2' is the smallest number in the array", () => {
-    //         let output = statsCalculator.validate([53, 88, 4, 2, 90, 100])
-
-    //         expect(output.minNum).toBe(2)
-    // })
-
-    // it("knows that '53' is the biggest number in the array", () => {
-    //     let output = statsCalculator.validate([53])
-
-    //     expect(output.maxNum).toBe(53)
-    // })
+        expect(output.maxNum).toBe(53)
+    })
 
     // it("knows '1' is the number of integers in the array", () => {
     //     let output = statsCalculator.validate([1])
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/statsCalculator.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/statsCalculator.ts
@@ -2,10 +2,11 @@ export class statsCalculator {
 
     public static validate (input: number[]) {
         const smallestInteger = input.reduce((min, current) => current < min ? current : min)
+        const biggestNumber = input.reduce((max, current) => current > max ? current : max)
 
         return {
             minNum: smallestInteger,
-            // maxNum: 53,
+            maxNum: biggestNumber,
             // arrNum: 1
         }
     }
