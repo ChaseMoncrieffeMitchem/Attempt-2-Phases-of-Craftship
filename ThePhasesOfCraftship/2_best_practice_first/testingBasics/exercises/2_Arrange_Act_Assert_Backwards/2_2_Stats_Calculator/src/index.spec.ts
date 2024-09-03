b46965656ec120dbefc509f596f1615f51e93b60
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 15:26:55 -0500

    [Refactor]: parameterization

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -35,19 +35,30 @@ describe('stats calculator', () => {
         })
     })
 
-    it("knows '18.66666666666668' is the average value of an array of numbers", () => {
-        let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
-        expect(output.avgNum).toBe(18.666666666666668)
+    describe("Identifying the average of an array of numbers", () => {
+        it.each([
+            [[2, 4, 21, -8, 53, 40], 18.666666666666668],
+            [[53, 88, 94, 70, 90, 100], 82.5],
+            [[53, 88, 4, 2, 90, 1300], 256.1666666666667]
+        ])("%s returns %s", (input: number[], result: number) => {
+            let output = statsCalculator.validate(input)
+            expect(output.avgNum).toBe(result)
+        })
     })
 
-    it("knows '82.5' is the average value of an array of numbers", () => {
-        let output = statsCalculator.validate([53, 88, 94, 70, 90, 100])
-        expect(output.avgNum).toBe(82.5)
-    })
+    // it("knows '18.66666666666668' is the average value of an array of numbers", () => {
+    //     let output = statsCalculator.validate([2, 4, 21, -8, 53, 40])
+    //     expect(output.avgNum).toBe(18.666666666666668)
+    // })
 
-    it("knows '256.1666666666667' is the average value of an array of numbers", () => {
-        let output = statsCalculator.validate([53, 88, 4, 2, 90, 1300])
-        expect(output.avgNum).toBe(256.1666666666667)
-    })
+    // it("knows '82.5' is the average value of an array of numbers", () => {
+    //     let output = statsCalculator.validate([53, 88, 94, 70, 90, 100])
+    //     expect(output.avgNum).toBe(82.5)
+    // })
+
+    // it("knows '256.1666666666667' is the average value of an array of numbers", () => {
+    //     let output = statsCalculator.validate([53, 88, 4, 2, 90, 1300])
+    //     expect(output.avgNum).toBe(256.1666666666667)
+    // })
 
 })
