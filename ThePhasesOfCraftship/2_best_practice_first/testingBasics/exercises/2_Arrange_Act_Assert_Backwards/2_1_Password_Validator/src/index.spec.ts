1253e9f2fb89a513c6939c9bccfc861733b8cf35
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-03 10:59:34 -0500

    [Red]: knows 'Mommy5' is a valid password bc character length is bw 5 and 15

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,3 @@
-import { passwordValidator } from "./passwordValidator"
-
 // 🔘 I have implemented the minimum requirements listed in the project description
 // 🔘 I have Programmed By Wishful Thinking, designing the response API before it was actually created 
 // 🔘 I have Worked Backwards, starting at the Assert, then going to the Act and the Arrange
@@ -9,27 +7,13 @@ import { passwordValidator } from "./passwordValidator"
 // "thePhysical1234567" returns a false-y response because of exceeding the 15 character length
 // 🔘 Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization if there is sufficient duplication (see Tip #3 here)
 // 🔘 There is no duplication in my test code or my production code
+import { passwordValidator } from "./passwordValidator";
+
 
 describe('password validator', () => {
 
-  it('returns true when "mom555" is bw 5 and 15 chars long', () => {
-    expect(passwordValidator("mom555")).toBe(true)
-  })
-
-  it('returns false when "mom" is less than 5 and 15 chars long', () => {
-    expect(passwordValidator("mom")).toBe(false)
-  })
-
-  it('returns false when "mommy" does not contain at least 1 digit', () => {
-    expect(passwordValidator("mommy")).toBe(false)
-  })
-
-  it('returns true when "mommy1" does contain at least 1 digit', () => {
-    expect(passwordValidator("mommy1")).toBe(true)
-  })
-
-  it('returns false when "mommy12" does not contain at least one uppercase letter', () => {
-    expect(passwordValidator("mommy12")).toBe(false)
+  it("knows 'Mommy5' is a valid password bc character length is bw 5 and 15", () => {
+    expect(passwordValidator("Mommy5")).toBe(true)
   })
 
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/passwordValidator.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/passwordValidator.ts
@@ -1,13 +1,3 @@
-export function passwordValidator(str: string) {
-    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    const passwordLength = str.length
-    const hasDigit = digits.some(digit => str.includes(digit))
-    const isValidLength = passwordLength >= 5 && passwordLength <= 15
-    // const hasUppercaseLetter = str !== str.toLowerCase()
-
-    // if (!hasUppercaseLetter) return false
-    if (!isValidLength) return false
-    if (!hasDigit) return false
-    return true
-
+export function passwordValidator (str: string) {
+    return false
 }
