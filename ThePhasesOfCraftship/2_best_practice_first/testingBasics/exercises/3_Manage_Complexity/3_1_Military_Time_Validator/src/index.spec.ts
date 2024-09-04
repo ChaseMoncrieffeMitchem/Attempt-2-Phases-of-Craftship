be1df8bf97b4b85d93a1a151285972ca224a96a4
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 14:46:36 -0500

    [Refactor]: refactored based on higher level abstraction that we want to make sure the time inputted is within the correct range of military times

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,28 +3,41 @@ import { militaryTime } from "./militaryTime";
 
 describe('military time validator', () => {
 
-    it("knows '25:00 - 12:23' is NOT valid military time because left side goes over '24:00'", () => {
-        let output = militaryTime.validate("25:00 - 12:23")
+    describe("Input is a valid military time format, checks if the input is within the valid military time range", () => {
+        it.each([
+            ["25:00 - 12:23", false],
+            ["22:00 - 26:23", false],
+            ["22:00 - 22:78", false],
+            ["00:00 - 23:59", true]
+        ])("knows that '%s' returns %s", (input: string, result: boolean) => {
+            let output = militaryTime.validate(input)
 
-        expect(output).toBeFalsy()
+            expect(output).toBe(result)
+        })
     })
 
-    it("knows '22:00 - 26:23' is NOT valid military time because right side goes over '24:00'", () => {
-        let output = militaryTime.validate("22:00 - 26:23")
+    // it("knows '25:00 - 12:23' is NOT valid military time because left side goes over '24:00'", () => {
+    //     let output = militaryTime.validate("25:00 - 12:23")
 
-        expect(output).toBeFalsy()
-    })
+    //     expect(output).toBeFalsy()
+    // })
 
-    it("knows '22:00 - 22:78' is NOT valid military time because right side minutes go over '00:59'", () => {
-        let output = militaryTime.validate("22:00 - 22:78")
+    // it("knows '22:00 - 26:23' is NOT valid military time because right side goes over '24:00'", () => {
+    //     let output = militaryTime.validate("22:00 - 26:23")
 
-        expect(output).toBeFalsy()
-    })
+    //     expect(output).toBeFalsy()
+    // })
 
-    it("knows '00:00 - 23:59' is a valid military time", () => {
-        let output = militaryTime.validate("00:00 - 23:59")
+    // it("knows '22:00 - 22:78' is NOT valid military time because right side minutes go over '00:59'", () => {
+    //     let output = militaryTime.validate("22:00 - 22:78")
 
-        expect(output).toBeTruthy()
-    })
+    //     expect(output).toBeFalsy()
+    // })
+
+    // it("knows '00:00 - 23:59' is a valid military time", () => {
+    //     let output = militaryTime.validate("00:00 - 23:59")
+
+    //     expect(output).toBeTruthy()
+    // })
 
 })
