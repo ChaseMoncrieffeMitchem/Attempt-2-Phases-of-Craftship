9c35aafd3839d03b63ebe8125fda3afdd9c2efae
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 16:05:45 -0500

    [Green]: knows '00:0' is not a valid time format

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -7,4 +7,12 @@ describe('military time validator', () => {
         expect(militaryTime.validate("00:00")).toBe(true)
     })
 
+    it("knows '000:00' is not a valid time format", () => {
+        expect(militaryTime.validate("000:00")).toBe(false)
+    })
+
+    it("knows '00:0' is not a valid time format", () => {
+        expect(militaryTime.validate("00:0")).toBeFalsy()
+    })
+
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,5 +1,10 @@
+const isValidTimeFormat = (input: string) => {
+    const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/
+    return timePattern.test(input)
+}
+
 export class militaryTime {
     public static validate (input: string) {
-        return true
+        return isValidTimeFormat(input)
     }
 }
