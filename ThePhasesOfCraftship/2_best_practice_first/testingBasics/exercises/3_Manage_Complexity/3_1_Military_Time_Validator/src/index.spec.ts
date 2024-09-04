0cb1264cbf28edcac351fa749b831b0e83295192
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 11:50:39 -0500

    [Green]: knows '25:00 - 12:23' is NOT valid military time because it goes over '24:00'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,12 @@
+import { militaryTime } from "./militaryTime";
+
 
 describe('military time validator', () => {
 
+    it("knows '25:00 - 12:23' is NOT valid military time because it goes over '24:00'", () => {
+        let output = militaryTime.validate("25:00 - 12:23")
+        
+        expect(output.result).toBeFalsy()
+    })
 
 })
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -0,0 +1,8 @@
+export class militaryTime {
+    public static validate (input: string) {
+
+        return {
+            result: false
+        }
+    }
+}
