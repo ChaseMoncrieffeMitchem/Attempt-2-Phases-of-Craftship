9fa689ca7df4f1b9c5a88a1205d3637170e14f9d
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 15:44:26 -0500

    [Green]: knows '00:00' is a valid time format

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,41 +3,8 @@ import { militaryTime } from "./militaryTime";
 
 describe('military time validator', () => {
 
-    describe("Input is a valid military time format, checks if the input is within the valid military time range", () => {
-        it.each([
-            ["25:00 - 12:23", false],
-            ["22:00 - 26:23", false],
-            ["22:00 - 22:78", false],
-            ["00:00 - 23:59", true]
-        ])("knows that '%s' returns %s", (input: string, result: boolean) => {
-            let output = militaryTime.validate(input)
-
-            expect(output).toBe(result)
-        })
+    it("knows '00:00' is a valid time format", () => {
+        expect(militaryTime.validate("00:00")).toBe(true)
     })
-
-    // it("knows '25:00 - 12:23' is NOT valid military time because left side goes over '24:00'", () => {
-    //     let output = militaryTime.validate("25:00 - 12:23")
-
-    //     expect(output).toBeFalsy()
-    // })
-
-    // it("knows '22:00 - 26:23' is NOT valid military time because right side goes over '24:00'", () => {
-    //     let output = militaryTime.validate("22:00 - 26:23")
-
-    //     expect(output).toBeFalsy()
-    // })
-
-    // it("knows '22:00 - 22:78' is NOT valid military time because right side minutes go over '00:59'", () => {
-    //     let output = militaryTime.validate("22:00 - 22:78")
-
-    //     expect(output).toBeFalsy()
-    // })
-
-    // it("knows '00:00 - 23:59' is a valid military time", () => {
-    //     let output = militaryTime.validate("00:00 - 23:59")
-
-    //     expect(output).toBeTruthy()
-    // })
 
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,17 +1,5 @@
 export class militaryTime {
     public static validate (input: string) {
-        const [leftTime, rightTime] = input.split(" - ")
-
-        const leftHour = +leftTime.split(":")[0]
-        const rightHour = +rightTime.split(":")[0]
-        const validLeftHour = +(leftHour >= 0 && leftHour <= 23)
-        const validRightHour = +(rightHour >= 0 && rightHour <= 23)
-        
-        const leftMinutes = +leftTime.split(":")[1]
-        const rightMinutes = +rightTime.split(":")[1]
-        const validLeftMinutes = +(leftMinutes >= 0 && leftMinutes <= 59)
-        const validRightMinutes = +(rightMinutes >= 0 && rightMinutes <= 59)
-
-        return (validLeftMinutes + validRightMinutes) === 2 && (validLeftHour + validRightHour) === 2
+        return true
     }
 }
