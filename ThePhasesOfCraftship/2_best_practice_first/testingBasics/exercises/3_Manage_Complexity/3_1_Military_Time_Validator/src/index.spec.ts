7d13b2e6117af23f14fe204609df790e0d137f25
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 12:47:23 -0500

    [Green]: moved my production code higher up the TPP chart constant to scalar then scalar to function arguments

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,10 +3,16 @@ import { militaryTime } from "./militaryTime";
 
 describe('military time validator', () => {
 
-    it("knows '25:00 - 12:23' is NOT valid military time because it goes over '24:00'", () => {
+    it("knows '25:00 - 12:23' is NOT valid military time because left side goes over '24:00'", () => {
         let output = militaryTime.validate("25:00 - 12:23")
 
         expect(output).toBeFalsy()
     })
+
+    // it("knows '22:00 - 26:23' is NOT valid military time because right side goes over '24:00'", () => {
+    //     let output = militaryTime.validate("22:00 - 26:23")
+
+    //     expect(output).toBeFalsy()
+    // })
 
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,10 +1,8 @@
 export class militaryTime {
     public static validate (input: string) {
-        const timeArray = input.split(" - ")
-        const leftSideArray = timeArray[0]
-        const hourLeftSideArray = leftSideArray.split(":")[0]
-    
-        if (+hourLeftSideArray > 24) return false
+        
+        const leftHour = +input.split(":")[0]
+        return leftHour <= 24
         
 
     }
