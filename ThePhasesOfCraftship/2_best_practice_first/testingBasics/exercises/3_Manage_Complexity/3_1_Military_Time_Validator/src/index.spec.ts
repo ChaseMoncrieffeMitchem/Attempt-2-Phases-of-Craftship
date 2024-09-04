58fc9f5c9fb6f1ffd06255d38116177d1cc5df66
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 12:29:35 -0500

    [Green]: knows '25:00 - 12:23' is NOT valid military time because left side goes over '24:00' with logic

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,8 +5,8 @@ describe('military time validator', () => {
 
     it("knows '25:00 - 12:23' is NOT valid military time because it goes over '24:00'", () => {
         let output = militaryTime.validate("25:00 - 12:23")
-        
-        expect(output.result).toBeFalsy()
+
+        expect(output).toBeFalsy()
     })
 
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,8 +1,11 @@
 export class militaryTime {
     public static validate (input: string) {
+        const timeArray = input.split(" - ")
+        const leftSideArray = timeArray[0]
+        const hourLeftSideArray = leftSideArray.split(":")[0]
+    
+        if (+hourLeftSideArray > 24) return false
+        
 
-        return {
-            result: false
-        }
     }
 }
