e5d3256606e89916bf73143f6d0d650406016ab2
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-04 14:37:37 -0500

    [Refactor]: refactored based on TPP principles and involving transformations that do not require any conditional logic

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,33 +1,17 @@
 export class militaryTime {
     public static validate (input: string) {
         const [leftTime, rightTime] = input.split(" - ")
-        const leftHour = +leftTime[0].split(":")[0]
-        const rightHour = +rightTime[1].split(":")[0]
 
+        const leftHour = +leftTime.split(":")[0]
+        const rightHour = +rightTime.split(":")[0]
+        const validLeftHour = +(leftHour >= 0 && leftHour <= 23)
+        const validRightHour = +(rightHour >= 0 && rightHour <= 23)
         
-        const isValidMinutes = (time: string) => {
-            const minutes = +time.split(":")[1]
-            return minutes >= 0 && minutes <= 59
-        }
+        const leftMinutes = +leftTime.split(":")[1]
+        const rightMinutes = +rightTime.split(":")[1]
+        const validLeftMinutes = +(leftMinutes >= 0 && leftMinutes <= 59)
+        const validRightMinutes = +(rightMinutes >= 0 && rightMinutes <= 59)
 
-        const isValidHours = (time: string) => {
-            const hours = +time.split(":")[0]
-            return hours >= 0 && hours <= 23
-        }
-        // const leftMinutes = +leftTime[0].split(":")[1]
-        // const rightMinutes = +rightTime[1].split(":")[1]
-        // console.log(rightMinutes)
-
-        // if (leftMinutes <= 59 && rightMinutes <= 59) {
-        //     return true
-        // }
-
-        // if (leftHour < 24 && rightHour < 24) {
-        //     return true
-        // }
-
-        return isValidMinutes(leftTime) && isValidMinutes(rightTime) && isValidHours(leftTime) && isValidHours(rightTime)
-
-
+        return (validLeftMinutes + validRightMinutes) === 2 && (validLeftHour + validRightHour) === 2
     }
 }
