8b32a86e96cae9c43ff3eb8511c65cb5e4bd4602
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 10:25:48 -0500

    [Green]: knows '05:00 - 03:00' is not valid military time range

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -17,24 +17,19 @@ describe('military time validator', () => {
         it.each([
             ["00:00 23:59", false],
             ["a00:00 - 23:59", false],
-            ["00:000 - 00:00", false]
+            ["00:000 - 00:00", false],
+            ["0000 - 0001", false]
         ])("knows '%s' returns %s", (input: string, result: boolean) => {
             expect(militaryTime.validate(input)).toBe(result)
         })
     })
 
-    // it("knows '00:00 23:59' is not a valid time format", () => {
-    //     expect(militaryTime.validate("00:00 23:59")).toBeFalsy()
-    // })
+    it("knows '05:00 - 03:00' is not valid military time range", () => {
+        expect(militaryTime.validate('05:00 - 03:00')).toBeFalsy()
+    })
 
-    // it("knows 'a00:00 - 23:59' is not a valid time format", () => {
-    //     expect(militaryTime.validate("a00:00 - 23:59")).toBeFalsy()
-    // })
-
-    // it("knows '00:000 - 00:00' is not a valid time format", () => {
-    //     expect(militaryTime.validate("00:000 - 00:00")).toBeFalsy()
-    // })
-
-
+    it("knows '23:59 - 03:59' is not valid military time range", () => {
+        expect(militaryTime.validate('23:59 - 03:59')).toBeFalsy()
+    })
 
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,11 +1,15 @@
 const isValidTimeFormat = (input: string) => {
     const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+    const [leftSide, rightSide] = input.split(" - ")
 
     if (!input.includes(" - ")) {
         return false
     }
 
-    const [leftSide, rightSide] = input.split(" - ")
+    if (+leftSide.split(":")[0] > +rightSide.split(":")[0]) {
+        return false
+    }
+    
 
     return timePattern.test(leftSide) && timePattern.test(rightSide)
 }
