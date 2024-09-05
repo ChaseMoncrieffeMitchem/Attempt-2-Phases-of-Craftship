17ee2a366da96e84160e141bba0db4dd28bd067c
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 09:57:09 -0500

    [Refactor]: refactored by parameterization

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,15 +3,26 @@ import { militaryTime } from "./militaryTime";
 
 describe('military time validator', () => {
 
-    it("knows '00:00 - 23:59' is valid military time", () => {
-        expect(militaryTime.validate("00:00 - 23:59")).toBeTruthy()
+    describe("Checking that time range is within valid military time range", () => {
+        it.each([
+            ["00:00 - 23:59", true],
+            ["00:03 - 16:59", true],
+            ["00:80 - 16:59", false]
+        ])("knows '%s' returns %s", (input: string, result: boolean) => {
+            expect(militaryTime.validate(input)).toBe(result)
+        })
     })
+    // it("knows '00:00 - 23:59' is valid military time", () => {
+    //     expect(militaryTime.validate("00:00 - 23:59")).toBeTruthy()
+    // })
 
-    it("knows '00:03 - 16:59' is valid military time", () => {
-        expect(militaryTime.validate("00:03 - 16:59")).toBeTruthy()
-    })
+    // it("knows '00:03 - 16:59' is valid military time", () => {
+    //     expect(militaryTime.validate("00:03 - 16:59")).toBeTruthy()
+    // })
 
-    it("knows '00:80 - 16:59' is not valid military time", () => {
-        expect(militaryTime.validate("00:80 - 16:59")).toBeFalsy()
-    })
+    // it("knows '00:80 - 16:59' is not valid military time", () => {
+    //     expect(militaryTime.validate("00:80 - 16:59")).toBeFalsy()
+    // })
+
+
 })
