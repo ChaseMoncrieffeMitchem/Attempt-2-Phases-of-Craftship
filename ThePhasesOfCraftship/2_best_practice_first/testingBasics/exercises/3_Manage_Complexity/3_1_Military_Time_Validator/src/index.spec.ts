1ff3b3e9aa0b85ba0c6d0232ea7ab07aeabca4ba
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 09:49:17 -0500

    [Green]: knows '00:03 - 16:59' is valid military time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -6,4 +6,8 @@ describe('military time validator', () => {
     it("knows '00:00 - 23:59' is valid military time", () => {
         expect(militaryTime.validate("00:00 - 23:59")).toBeTruthy()
     })
+
+    it("knows '00:03 - 16:59' is valid military time", () => {
+        expect(militaryTime.validate("00:03 - 16:59")).toBeTruthy()
+    })
 })
