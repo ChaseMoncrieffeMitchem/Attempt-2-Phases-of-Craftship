bfce6e28745fbe624272e64c73c8bc44bef5a1dc
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 11:23:22 -0500

    [Green]: knows 'FALSE' returns false

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/booleanCalc.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/booleanCalc.ts
@@ -2,7 +2,8 @@
 
 export class booleanCalc {
     public static validate(input: string) {
+        const turnToBoolean = input === "TRUE" ? true : false
 
-        return input
+        return turnToBoolean
     }
 }
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -2,7 +2,14 @@ import { booleanCalc } from "./booleanCalc";
 
 
 describe('boolean calculator', () => {
+   
     it("knows 'TRUE' returns true", () => {
         expect(booleanCalc.validate("TRUE")).toBeTruthy()
     })
+
+    it("knows 'FALSE' returns false", () => {
+        expect(booleanCalc.validate("FALSE")).toBeFalsy()
+    })
+
+
 })
