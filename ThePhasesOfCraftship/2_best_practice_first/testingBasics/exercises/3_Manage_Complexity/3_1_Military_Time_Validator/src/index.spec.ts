357952be5b972a9710d7ff6dba20346c1a7d1878
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 09:47:13 -0500

    [Green]: knows '00:00 - 23:59' is valid military time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,20 +3,7 @@ import { militaryTime } from "./militaryTime";
 
 describe('military time validator', () => {
 
-    it("knows '00:00' is a valid time format", () => {
-        expect(militaryTime.validate("00:00")).toBe(true)
+    it("knows '00:00 - 23:59' is valid military time", () => {
+        expect(militaryTime.validate("00:00 - 23:59")).toBeTruthy()
     })
-
-    it("knows '000:00' is not a valid time format", () => {
-        expect(militaryTime.validate("000:00")).toBe(false)
-    })
-
-    it("knows '00:0' is not a valid time format", () => {
-        expect(militaryTime.validate("00:0")).toBeFalsy()
-    })
-
-    it("knows '00:00 00:00' is not a valid time range", () => {
-        expect(militaryTime.validate("00:00 00:00")).toBeFalsy()
-    })
-
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/militaryTime.ts
@@ -1,10 +1,20 @@
 const isValidTimeFormat = (input: string) => {
-    const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/
-    return timePattern.test(input)
+    const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
+    if (!input.includes(" - ")) {
+        return false
+    }
+
+    const [leftSide, rightSide] = input.split(" - ")
+
+    return timePattern.test(leftSide) && timePattern.test(rightSide)
 }
 
+
 export class militaryTime {
-    public static validate (input: string) {
+    public static validate(input: string) {
+        
         return isValidTimeFormat(input)
     }
 }
+
