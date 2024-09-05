37742f008a2658dba223841922b801846c4862f8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-09-05 09:59:07 -0500

    [Green]: knows '00:00 23:59' is not a valid time format

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -12,17 +12,9 @@ describe('military time validator', () => {
             expect(militaryTime.validate(input)).toBe(result)
         })
     })
-    // it("knows '00:00 - 23:59' is valid military time", () => {
-    //     expect(militaryTime.validate("00:00 - 23:59")).toBeTruthy()
-    // })
 
-    // it("knows '00:03 - 16:59' is valid military time", () => {
-    //     expect(militaryTime.validate("00:03 - 16:59")).toBeTruthy()
-    // })
-
-    // it("knows '00:80 - 16:59' is not valid military time", () => {
-    //     expect(militaryTime.validate("00:80 - 16:59")).toBeFalsy()
-    // })
-
+    it("knows '00:00 23:59' is not a valid time format", () => {
+        expect(militaryTime.validate("00:00 23:59")).toBeFalsy()
+    })
 
 })
