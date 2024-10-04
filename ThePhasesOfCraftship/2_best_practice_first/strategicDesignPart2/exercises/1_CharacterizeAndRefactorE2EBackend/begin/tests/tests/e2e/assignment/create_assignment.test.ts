54c8a14969abc7151492c478872fcea536f9fe54
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-04 13:46:38 -0500

    Working to make my assignemt creation fail because of duplicate title, have to work through hoisting certain variables so I can pass them into the assignmentInput and test the correct collision

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/e2e/assignment/create_assignment.test.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/e2e/assignment/create_assignment.test.ts
@@ -8,8 +8,8 @@ import { assignmentBuilder } from "../../builders/assignment/createAssignment Bu
 import { Server } from "http";
 
 function getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }  
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const feature = loadFeature(
   path.join(__dirname, "../../features/create_assignment.feature")
@@ -38,31 +38,68 @@ defineFeature(feature, (test) => {
     });
 
     given(/^I want to create a assignment titled "(.*)"$/, async (arg0) => {
-        const randomInteger = getRandomNumber(100, 10000)
-      response = await driver.post('/classes', { name: `Math ${randomInteger}` })
-      classId = response.body.data.id
-      console.log("I'm right here:", classId)
-      assignmentInput = new assignmentBuilder().withTitle().withClassId(classId).build();
+      const randomInteger = getRandomNumber(100, 10000);
+      response = await driver.post("/classes", {
+        name: `Math ${randomInteger}`,
+      });
+      classId = response.body.data.id;
+      console.log("I'm right here:", classId);
+      assignmentInput = new assignmentBuilder()
+        .withTitle()
+        .withClassId(classId)
+        .build();
     });
 
     when("I request to create that assignment", async () => {
       response = await driver.post("/assignments", assignmentInput);
-      console.log(response)
     });
-
 
     then("the assignment should be Successfully created", () => {
       expect(response.body.success).toBeTruthy();
       expect(response.body.error).toBeFalsy();
-      expect(response.body.data.title).toEqual(assignmentInput.title)
+      expect(response.body.data.title).toEqual(assignmentInput.title);
     });
   });
 
   test("Failed to create a assignment", ({ given, when, then }) => {
-    given(/^a assignment by title "(.*)" already exists$/, (arg0) => {});
+    let assignmentInput: createAssignmentDTO;
+    let root = new CompositionRoot();
+    let webServer: WebServer = root.getWebServer();
+    let driver: RESTfulAPIDriver;
+    let response: any;
+    let classId: any;
 
-    when("I request to create a assignment by that same title", () => {});
+    beforeEach(async () => {
+      // Start the Server
+      await webServer.start(3005);
 
-    then("the assignment should not be created", () => {});
+      driver = new RESTfulAPIDriver(webServer.getHttp() as Server, 3005);
+      // Reset the database
+    });
+
+    afterEach(async () => {
+      // Stop the processes running on the Server
+      await webServer.stop();
+    });
+
+
+    given(/^a assignment by title "(.*)" already exists$/, async (arg0) => {
+        const randomInteger = getRandomNumber(100, 10000);
+        response = await driver.post("/classes", {
+            name: `Math ${randomInteger}`,
+          });
+          const classId = response.body.data.id
+      assignmentInput = new assignmentBuilder().withTitle().withClassId(classId).build();
+      response = await driver.post("/assignments", assignmentInput)
+      console.log(response)
+    });
+
+    when("I request to create a assignment by that same title", async () => {
+      response = await driver.post("/assignments", assignmentInput);
+    });
+
+    then("the assignment should not be created", () => {
+      expect(response.body.success).toBeFalsy();
+    });
   });
 });
