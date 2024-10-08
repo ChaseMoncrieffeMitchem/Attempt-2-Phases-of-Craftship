c2bf2ce1989c3521e24d09e460a53cd4d9da7a6f
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-08 16:50:12 -0500

    Added failure test case for assigning an assignment to a student but the assignment does not exist, the test was passing but was exiting with a forced exit. I learned if I use BeforeAll and AfterAll they need to be outside of both of my tests in the feature file, and different ports are not necessary

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/e2e/student/mergeStudentWithAssignment.test.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/e2e/student/mergeStudentWithAssignment.test.ts
@@ -15,38 +15,41 @@ import { assignmentBuilder } from "../../builders/assignment/createAssignment Bu
 import { createAssignmentDTO } from "../../../../src/shared/dtos/assignment/create_assignmentDTO";
 import { StudentAssignmentBuilder } from "../../builders/student/createStudentAssignmentBuilder";
 import { createStudentAssignmentDTO } from "../../../../src/shared/dtos/student/createStudentAssignmentDTO";
+import { EnrollmentBuilder } from "../../builders/class/enrollmentBuilder";
 
 const feature = loadFeature(
   path.join(__dirname, "../../features/connect_student_with_assignment.feature")
 );
 
 defineFeature(feature, (test) => {
+  let studentId: any;
+  let requestBody: any = {};
+  let root = new CompositionRoot();
+  let webServer: WebServer = root.getWebServer();
+  let driver: RESTfulAPIDriver;
+  let response: any;
+
+  beforeAll(async () => {
+    await webServer.start(3012);
+    driver = new RESTfulAPIDriver(webServer.getHttp() as Server, 3012);
+  });
+
+  afterAll(async () => {
+    await webServer.stop();
+  });
+
   test("Successfully give Assignment to Student", ({
     given,
     and,
     when,
     then,
   }) => {
-    let response: any;
-    let root = new CompositionRoot();
-    let webServer: WebServer = root.getWebServer();
-    let driver: RESTfulAPIDriver;
     let classInput: createClassDTO;
     let classId: any;
-    let studentId: any;
     let assignmentInput: createAssignmentDTO;
     let assignmentId: any;
     let studentWithAssignmentInput: createStudentAssignmentDTO;
     let studentInput: createStudentDTO;
-
-    beforeAll(async () => {
-      await webServer.start(3012);
-      driver = new RESTfulAPIDriver(webServer.getHttp() as Server, 3012);
-    });
-
-    afterAll(async () => {
-      await webServer.stop();
-    });
 
     given("a student exists", async () => {
       studentInput = new StudentBuilder()
@@ -84,11 +87,32 @@ defineFeature(feature, (test) => {
         "/student-assignments",
         studentWithAssignmentInput
       );
-      console.log(response);
     });
 
     then("the student should be successfully assigned that Assignement", () => {
       expect(response.statusCode).toBe(201);
+    });
+  });
+
+  test("Failed to give an Assignment to Student b/c assignment Does Not Exist", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("an assignment Does not exist", () => {
+      requestBody = {
+        studentId: studentId,
+        assignmentId: "",
+      };
+    });
+
+    when("I request to assign that non-assignment to the student", async () => {
+      response = await driver.post("/class-enrollments", requestBody);
+    });
+
+    then("the student should not be assigned that Assignement", () => {
+      expect(response.statusCode).toBe(400);
+      expect(response.body.error).toBeDefined();
     });
   });
 });
--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/features/connect_student_with_assignment.feature
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/features/connect_student_with_assignment.feature
@@ -9,3 +9,8 @@ Feature: Give an Assignement to a Student
         And an assignment exists
         When I request to assign that assignment to the student
         Then the student should be successfully assigned that Assignement
+
+    Scenario: Failed to give an Assignment to Student b/c assignment Does Not Exist
+        Given an assignment Does not exist
+        When I request to assign that non-assignment to the student
+        Then the student should not be assigned that Assignement
