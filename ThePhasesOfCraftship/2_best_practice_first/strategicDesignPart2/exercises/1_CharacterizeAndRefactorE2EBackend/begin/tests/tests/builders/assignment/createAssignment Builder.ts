a4b92e7632b8c175590bee75b9940c2dea6b53f8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-09 12:57:34 -0500

    Actually finished the create assignment Builder. Had to move the API call to the build section for better maintainability, readability, and flexibility. Had to understand the withAssignmentId method is only being used to capture the value if it is already given, the build needs to be doing the work of calling to the API

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/assignment/createAssignment Builder.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/assignment/createAssignment Builder.ts
@@ -1,64 +1,70 @@
-import { createAssignmentDTO } from "../../../../src/shared/dtos/assignment/create_assignmentDTO"
+import { createAssignmentDTO } from "../../../../src/shared/dtos/assignment/create_assignmentDTO";
 import { RESTfulAPIDriver } from "../../../../src/shared/http/apiDriver";
 import { ClassBuilder } from "../class/createClassBuilder";
 
 function getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }  
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export class assignmentBuilder {
-    private assignmentInput: createAssignmentDTO
-    private classBuilder?: ClassBuilder
-    private driver: RESTfulAPIDriver
+  private assignmentInput: createAssignmentDTO;
+  private classBuilder?: ClassBuilder;
+  private driver: RESTfulAPIDriver;
 
-    constructor(driver: RESTfulAPIDriver) {
-        this.assignmentInput = {
-            title: "",
-            classId: "",
-            assignmentId: ''
-        }
-        this.driver = driver
+  constructor(driver: RESTfulAPIDriver) {
+    this.assignmentInput = {
+      title: "",
+      classId: "",
+      assignmentId: "",
+    };
+    this.driver = driver;
+  }
+
+  from(classBuilder: ClassBuilder) {
+    this.classBuilder = classBuilder;
+    return this;
+  }
+
+  withTitle(value: string) {
+    if (value) {
+      this.assignmentInput.title = value; // Set the title if a value is provided
+    } else {
+      const randomInteger = getRandomNumber(100, 10000);
+      this.assignmentInput.title = `title-${randomInteger}`; // Generate a random title if value is empty
+    }
+    return this;
+  }
+
+  async withClassId() {
+    if (this.classBuilder) {
+      const classOutput = await this.classBuilder.build();
+      this.assignmentInput.classId = classOutput.classId;
+    } else {
+      throw new Error("ClassBuilder is not defined.");
+    }
+    return this;
+  }
+
+  withAssignmentId(value: string) {
+    if (value) {
+      this.assignmentInput.assignmentId = value;
+    }
+    return this;
+  }
+
+  async build(): Promise<createAssignmentDTO> {
+    if (!this.assignmentInput.classId) {
+      await this.withClassId();
     }
 
-    from (classBuilder: ClassBuilder) {
-        this.classBuilder = classBuilder
-        return this
+    if (!this.assignmentInput.assignmentId) {
+      const response = await this.driver.post("/assignments", {
+        title: this.assignmentInput.title,
+        classId: this.assignmentInput.classId,
+      });
+      this.assignmentInput.assignmentId = response.body.data?.id;
     }
 
-    withTitle(value: string) {
-        if (value) {
-            this.assignmentInput.title = value; // Set the title if a value is provided
-        } else {
-            const randomInteger = getRandomNumber(100, 10000);
-            this.assignmentInput.title = `title-${randomInteger}`; // Generate a random title if value is empty
-        }
-        return this;
-    }
-    
-
-    async withClassId() {
-        if (this.classBuilder) {
-            const classOutput = await this.classBuilder.build(); 
-            this.assignmentInput.classId = classOutput.classId;
-        } else {
-            throw new Error('ClassBuilder is not defined.');
-        }
-        return this;
-    }
-
-    async withAssignmentId(value: string) {
-        if (value) {
-            return this
-        } else {
-            const response = await this.driver.post('/assignments', { title: this.assignmentInput.title, classId: this.assignmentInput.classId })
-        }
-    }
-
-    async build(): Promise<createAssignmentDTO> {
-        return {
-            title: this.assignmentInput.title,
-            classId: this.assignmentInput.classId,
-            assignmentId: this.assignmentInput.assignmentId
-        }
-    }
+    return this.assignmentInput
+  }
 }
