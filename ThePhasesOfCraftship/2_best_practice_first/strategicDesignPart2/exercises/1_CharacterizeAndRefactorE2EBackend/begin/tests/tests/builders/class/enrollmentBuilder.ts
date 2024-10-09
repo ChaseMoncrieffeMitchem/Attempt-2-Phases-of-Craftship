ad8aefb6f797e225db4465dcc657f06aefb9aaa8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-09 12:07:37 -0500

    after completing studentBuilder I could complete my enrollment builder and have it post to the server with the studentId and classId I have from the respective Builders.

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/class/enrollmentBuilder.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/class/enrollmentBuilder.ts
@@ -1,30 +1,83 @@
-import { createEnrolledStudentDTO } from "../../../../src/shared/dtos/class/enrolledStudentDTO";
-import { StudentBuilder } from "../../builders/student/createStudentBuilder";
-import { ClassBuilder } from "../../builders/class/createClassBuilder";
+// import { createEnrolledStudentDTO } from "../../../../src/shared/dtos/class/enrolledStudentDTO";
+// import { StudentBuilder } from "../../builders/student/createStudentBuilder";
+// import { ClassBuilder } from "../../builders/class/createClassBuilder";
 
-export class EnrollmentBuilder {
-  private enrollmentInput: createEnrolledStudentDTO;
+// export class EnrollmentBuilder {
+//   private enrollmentInput: createEnrolledStudentDTO;
 
-  constructor() {
-    this.enrollmentInput = {
-      studentId: "",
-      classId: "",
-    };
+//   constructor() {
+//     this.enrollmentInput = {
+//       studentId: "",
+//       classId: "",
+//     };
+//   }
+
+//   withStudentId(value: string) {
+//     // const student = new StudentBuilder().withName("").withRandomEmail("").withStudentId("").build();
+//     this.enrollmentInput.studentId = value;
+//     return this;
+//   }
+
+//   withClassId(value: string) {
+//     // const classObj = new ClassBuilder().withName("").withClassId("").build();
+//     this.enrollmentInput.classId = value;
+//     return this;
+//   }
+
+//   build(): createEnrolledStudentDTO {
+//     return this.enrollmentInput;
+//   }
+// }
+
+import { RESTfulAPIDriver } from "../../../../src/shared/http/apiDriver";
+import { StudentBuilder } from "../student/createStudentBuilder";
+import { ClassBuilder } from "./createClassBuilder";
+
+export class EnrolledStudentBuilder {
+  private classBuilder?: ClassBuilder;
+  private studentBuilder?: StudentBuilder;
+  private driver: RESTfulAPIDriver;
+
+  constructor(driver: RESTfulAPIDriver) {
+    this.driver = driver
   }
 
-  withStudentId(value: string) {
-    // const student = new StudentBuilder().withName("").withRandomEmail("").withStudentId("").build();
-    this.enrollmentInput.studentId = value;
+  from (classBuilder: ClassBuilder) {
+    this.classBuilder = classBuilder;
     return this;
   }
 
-  withClassId(value: string) {
-    // const classObj = new ClassBuilder().withName("").withClassId("").build();
-    this.enrollmentInput.classId = value;
+  and (studentBuilder: StudentBuilder) {
+    this.studentBuilder = studentBuilder;
     return this;
   }
 
-  build(): createEnrolledStudentDTO {
-    return this.enrollmentInput;
+  async build() {
+    if (!this.studentBuilder) throw new Error('You must define the student builder');
+    if (!this.classBuilder) throw new Error('You must define the classroom builder');
+
+    let classId = (await this.classBuilder.build()).classId
+    let studentId = (await this.studentBuilder.build()).studentId
+
+    const enrolledStudent = await this.driver.post('/class-enrollments', {classId, studentId} )
+    
+    // const enrolledStudent = await prisma.classEnrollment.upsert({
+    //   where: {
+    //     studentId_classId: {
+    //       studentId: student.id,
+    //       classId: classRoom.id
+    //     }
+    //   },
+    //   create: {
+    //     studentId: student.id,
+    //     classId: classRoom.id
+    //   },
+    //   update: {
+    //     studentId: student.id,
+    //     classId: classRoom.id
+    //   }
+    // });
+
+    return { enrolledStudent }
   }
 }
