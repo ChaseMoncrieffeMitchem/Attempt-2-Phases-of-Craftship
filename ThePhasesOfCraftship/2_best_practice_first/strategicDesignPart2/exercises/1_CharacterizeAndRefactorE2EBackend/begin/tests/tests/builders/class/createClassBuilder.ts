97c16dbc9514f48f932b528a586bd9eeafcc8eb2
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-09 11:53:44 -0500

    I've moved the logic of posting to the API from the test file to the inner layer concept of a Builder. That way my test file reads more like English in a declarative fashion. Just finished the ClassBuilder to this effect

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/class/createClassBuilder.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/tests/builders/class/createClassBuilder.ts
@@ -5,38 +5,45 @@ import { RESTfulAPIDriver } from "../../../../src/shared/http/apiDriver";
 import { StudentBuilder } from "../student/createStudentBuilder";
 
 function getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }  
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export class ClassBuilder {
-    private classInput: createClassDTO
-    private name: string = ""
-    private classId: string = ''
+  private classInput: createClassDTO;
+  private driver: RESTfulAPIDriver;
 
-    constructor() {
-        this.classInput = {
-            name: '',
-            classId: ''
-        }
-    }
+  constructor(driver: RESTfulAPIDriver) {
+    this.classInput = {
+      name: "",
+      classId: "",
+    };
+    this.driver = driver;
+  }
 
-    withName (value: string) {
-    if (value !== "") return this
-    const randomInteger = getRandomNumber(100, 100000)
-    this.classInput.name = `ClassName-${randomInteger}`
-    return this
-   }
-
-   withClassId(value: string) {
-    this.classInput.classId = value;
+  withName(value: string) {
+    if (value !== "") return this;
+    const randomInteger = getRandomNumber(100, 100000);
+    this.classInput.name = `ClassName-${randomInteger}`;
     return this;
   }
 
-   build(): createClassDTO {
-    return {
+  async withClassId(value: string) {
+    if (value) {
+      this.classInput.classId = value;
+    } else {
+      // Call API to create class and get classId if not provided
+      const response = await this.driver.post("/classes", {
         name: this.classInput.name,
-        classId: this.classInput.classId
+      });
+      this.classInput.classId = response.body.data?.id;
     }
-   }
-}
+    return this;
+  }
 
+  async build(): Promise<createClassDTO> {
+    if (!this.classInput.classId) {
+      await this.withClassId(""); // This ensures we get a classId from API if not provided
+    }
+    return this.classInput;
+  }
+}
