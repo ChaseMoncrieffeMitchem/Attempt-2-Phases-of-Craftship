586c38658a841e66df27dd14301459ff72f555ee
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-10-24 09:46:36 -0500

    built out a user dto that actually handles some validation logic

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart3/assignment/begin/packages/shared/dtos/user/createUserDTO.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart3/assignment/begin/packages/shared/dtos/user/createUserDTO.ts
@@ -1,7 +1,31 @@
-export type createUserDTO = {
-    email: string,
-    firstName: string,
-    lastName: string,
-    username: string,
-    password: string,
+import { isMissingKeys } from "@dddforum/shared/utils/utils";
+import { InvalidRequestBodyException } from "@dddforum/shared/errorsAndExceptions/exceptions";
+
+export class CreateUserDTO {
+    constructor(
+        public email: string,
+        public firstName: string,
+        public lastName: string,
+        public username: string,
+        public password: string
+    ) {}
+
+    static formRequest(body: unknown) {
+        const requiredKeys = ["email", "firstName", "lastName", "username", "password"];
+        const isRequestInvalid = !body || typeof body !== "object" || isMissingKeys(body, requiredKeys);
+
+        if (isRequestInvalid) {
+            throw new InvalidRequestBodyException(requiredKeys);
+        }
+
+        const { email, firstName, lastName, username, password } = body as {
+            email: string;
+            firstName: string;
+            lastName: string;
+            username: string;
+            password: string;
+        };
+
+        return new CreateUserDTO(email, firstName, lastName, username, password);
+    }
 }
